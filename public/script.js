1a4750997fd39f1850ea7b491ee08d60b500b911
Rukshan Dangalla <rukshandangalla@gmail.com>
2017-06-19 13:42:40 +0530

    development completed

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
 var PRICE = 9.99;
+var LOADNUM = 10;
 
 new Vue({
     el: "#app",
@@ -6,12 +7,24 @@ new Vue({
         total: 0,
         items: [],
         cart: [],
+        results: [],
         search: "anime",
         lastSearch: "",
         loading: false,
         price: PRICE
     },
+    computed:{
+        noMoreItems: function(){
+            return this.items.length === this.results.length && this.results.length > 0;
+        }
+    },
     methods: {
+        appendItems: function () {
+            if (this.items.length < this.results.length) {
+                var append = this.results.slice(this.items.length, this.items.length + LOADNUM);
+                this.items = this.items.concat(append);
+            }
+        },
         addItem: function (index) {
 
             var item = this.items[index];
@@ -51,16 +64,17 @@ new Vue({
                 }
             }
         },
-        onSubmit: function(){
+        onSubmit: function () {
             this.items = [];
             this.loading = true;
             this.$http
-            .get('/search/'.concat(this.search))
-            .then(function(res){
-                this.lastSearch = this.search;
-                this.items = res.data;
-                this.loading = false;
-            });
+                .get('/search/'.concat(this.search))
+                .then(function (res) {
+                    this.lastSearch = this.search;
+                    this.results = res.data;
+                    this.appendItems();
+                    this.loading = false;
+                });
         }
     },
     filters: {
@@ -68,7 +82,16 @@ new Vue({
             return '$'.concat(price.toFixed(2));
         }
     },
-    mounted: function(){
+    mounted: function () {
         this.onSubmit();
+
+        var vueInstance = this;
+
+        var elem = document.getElementById("product-list-bottom");
+        var watcher = scrollMonitor.create(elem);
+
+        watcher.enterViewport(function () {
+            vueInstance.appendItems();
+        });
     }
 });
